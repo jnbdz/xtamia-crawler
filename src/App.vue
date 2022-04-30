b87d0c4f3e351da882581f87a5c591e74d7d6966
jn <jn@kali.kali>
2022-04-30 10:00:03 -0400

    Last step for moving from vue-app to root directory.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HelloWorld msg="Welcome to XTamia"/>
+</template>
+
+<script>
+import HelloWorld from './components/CrawlerForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/CrawlerForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="crawler-form">
+        <form>
+            <input type="text" name="url"/>
+            <input type="submit" name="submit-url" value="Crawl"/>
+        </form>
+    </div>
+</template>
+
+<script>
+/*import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})*/
+</script>
+
+<style scoped>
+input {
+    width: 100%;
+}
+</style>
